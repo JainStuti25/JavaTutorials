84940c4c35bd8cb4c1a91579e87798679a035444
Stuti Jain <41702974+JainStuti25@users.noreply.github.com>
2020-01-04 23:16:25 +0530

    Create StringBuilderTest.java

---
+++ StringBuilderTest.java
@@ -0,0 +1,9 @@
+public class StringBuilderTest{
+  public static void main(String[] args){
+  
+    String s1 = "Hello";
+    StringBuilder sb = new StringBuilder(s1);
+    sb.append(" World");
+    System.out.println(sb);
+  }
+  }
